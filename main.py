5f943f145b5b847d66a54ab948fc2d7927b03a55
teereebee <thierry@barbedette.com>
2026-03-07 02:13:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/health')
 def hello():
     return "🚀 Mingus Horoscope Cron READY ! 🐕"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
