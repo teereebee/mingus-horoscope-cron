236327fe191acc9f6a15aa8c2f395649aae83969
teereebee <thierry@barbedette.com>
2026-03-07 02:47:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "🚀 Mingus Horoscope Cron READY ! 🐕"
-
-@app.route('/health')
-def health():
-    return "OK"
+@app.route("/")
+def home():
+    return "🚀 Mingus Horoscope V2 OK ! 🐕"
