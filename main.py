6deb498bbf42198d523c7db1e8d16865157d040b
teereebee <thierry@barbedette.com>
2026-03-09 13:54:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🐕 Mingus Horoscope OK"
+    return "🐕 Mingus Horoscope OK !"
 
 @app.route("/send-horoscope")
 def send_horoscope():
-    api_key = os.environ
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render default = 10000
-    app.run(host="0.0.0.0", port=port, debug=False)
+    api_key = os.environ.get("SENDGRID_API_KEY")
+    
+    if not api
